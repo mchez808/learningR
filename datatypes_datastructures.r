ade2c19552f45a6893f83d574f0fe540f8783eb4
KMQC <kmqc@novonordisk.com>
2021-01-08 14:43:48 -0800

    add basic data types

---
+++ datatypes_datastructures.r
@@ -0,0 +1,13 @@
+# DATA TYPES ###############################################
+# (double precision by default)
+x = 15
+typeof(x)  # "double"
+
+# "character"
+x = "c"
+xyz = 'abc'  # another "character"
+
+# "logical"
+x = TRUE
+#same type
+x = c(T,T,F)
