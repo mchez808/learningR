2dc00b3f05cce273ff80009ab960f706f2f38bc8
KMQC <kmqc@novonordisk.com>
2021-01-09 10:08:38 -0800

    coercing (char, int, data frames)

--- datatypes_datastructures.r
+++ datatypes_datastructures.r
@@ -57,3 +57,47 @@ mat  # Coerces all values to most basic data type
 df2 = as.data.frame(cbind(vNums, vChars))
 df2  # Makes a data frame with different data types
 is.data.frame(df2)
+
+## List ====================================================
+
+o1 <- c(1, 2, 3)
+o2 <- c("a", "b", "c", "d")
+list1 = list(o1, o2)
+list1
+
+list2 <- list(o1, o2, list1)  # Lists within lists
+list2
+
+
+# COERCING TYPES ###########################################
+
+## Automatic Coercion ======================================
+
+# Goes to "least restrictive" data type
+
+(coerce1 <- c(1, "b", TRUE))
+typeof(coerce1)  # 'character', in this case
+
+## Coerce Numeric to Integer ===============================
+
+(coerce2 <- 5)
+typeof(coerce2)
+
+(coerce3 <- as.integer(5))
+typeof(coerce3)
+
+## Coerce Character to Numeric =============================
+
+(coerce4 <- c("1", "2", "3"))
+typeof(coerce4)
+
+(coerce5 <- as.numeric(c("1", "2", "3")))
+typeof(coerce5)
+
+## Coerce Matrix to Data Frame =============================
+
+(coerce6 <- matrix(1:9, nrow= 3))
+is.matrix(coerce6)
+
+(coerce7 <- as.data.frame(matrix(1:9, nrow= 3)))
+is.data.frame(coerce7)
