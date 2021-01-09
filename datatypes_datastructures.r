a35e19d52bff42762c3aa7783f2fbabf9db2c6be
KMQC <kmqc@novonordisk.com>
2021-01-09 09:46:20 -0800

    add structs: vectors and matrices

--- datatypes_datastructures.r
+++ datatypes_datastructures.r
@@ -11,3 +11,35 @@ xyz = 'abc'  # another "character"
 x = TRUE
 #same type
 x = c(T,T,F)
+
+
+# DATA STRUCTURES ##########################################
+
+## Vector ==================================================
+v = c(1,2,3)
+is.vector(v)
+
+v = c(T,T,F)
+
+## Matrix ==================================================
+
+m = matrix(c(T,T,F,F,T,F), nrow = 2)
+is.matrix(m)
+
+m = matrix(c('a', 'b',
+             'c', 'd'),
+           nrow = 2,
+           byrow = T)
+
+m = matrix(c('a', 'b',
+             'c', 'd'),
+           nrow = 2)
+# default: byrow = FALSE (fills by column; 
+# when column full, moves on to next column)
+
+## Array ===================================================
+
+# Give data, then dimensions (rows, columns, tables)
+arr = array(c(1:24), c(4, 3, 2))
+# 3D array
+
